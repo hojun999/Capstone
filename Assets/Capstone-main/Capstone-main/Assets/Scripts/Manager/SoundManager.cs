668c9cd212ff2b917a866d5a29ecc798d84e654b
kim247428 <145660814+kim247428@users.noreply.github.com>
2023-09-25 19:20:38 +0900

    0925

---
+++ Assets/Capstone-main/Capstone-main/Assets/Scripts/Manager/SoundManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    //ΩÃ±€≈Ê
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
